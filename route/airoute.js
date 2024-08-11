5d50baf4d0ffc05c489b198a18614baf0108d15b
Anuj Singh Negi <anujnegi157@gmail.com>
2024-08-11 12:49:35 +0530

    api added

--- route/airoute.js
+++ route/airoute.js
@@ -1,15 +1,25 @@
 import express from "express";
 import { verifyJWT } from "../utils/verifyJWT.js";
-import { GptResponse , GeminiResponse } from "../controller/ai.controller.js";
+import {
+  GptResponse,
+  GeminiResponse,
+  GptResponse2,
+  llama,
+} from "../controller/ai.controller.js";
 import { upload } from "../utils/Upload.js";
 const AIRoute = express.Router();
 
+AIRoute.post("/v1/ai/gpt", verifyJWT, GptResponse);
 
-AIRoute.post('/v1/ai/gpt',verifyJWT,GptResponse)
+AIRoute.post(
+  "/v1/ai/gemini",
+  verifyJWT,
+  upload.single("file1"),
+  GeminiResponse
+);
 
-AIRoute.post('/v1/ai/gemini',verifyJWT,upload.single('file1'),GeminiResponse);
+AIRoute.post("/v1/ai/gptmini", verifyJWT, GptResponse2);
 
-
-
+AIRoute.post("/v1/ai/lama", verifyJWT, llama);
 
 export default AIRoute;
